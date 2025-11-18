44d81cf4b221f7ef41ddffa998be222e12cd42cd
salmarashedy <s.ahmed2378@nu.edu.eg>
2025-11-18 18:40:19 +0200

    Delete session 5 directory

--- session 5/bin/pro.dart
+++
@@ -1,8 +0,0 @@
-import 'package:session_5/Book.dart' as session_5;
-
-void main() {
-  session_5.Book s = session_5.Book(name:"book 1",price: 100, discount: 10);
-  s.finalprice();
-  session_5.Book s1 = session_5.Book(name:"book 2",price: 100);
-  s1 .finalprice();
-}
